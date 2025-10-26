69bbe5bddbad847a4ec8979b3d39219fa0403809
Talal2003 <www.talal2003@gmail.com>
2025-10-25 22:57:54 -0400

    Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
+import Hero from './assets/Components/Hero/Hero'
 import Navbar from './assets/Components/Navbar/Navbar'
 
 function App() {
   return <>
-  
   <Navbar/>
-
+  <Hero/>
   </>
 }
 
---
+++ src/assets/Components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import style from './/hero.module.css'
+import portfolioIcon from '../../svg/avataaars.svg'
+
+function Hero() {
+    return <>
+    <div className="bg__primary">
+        <div className="container d-flex flex-column align-items-center gap-3 p-5">
+            <img className={style.portfolioIcon} src={portfolioIcon} alt="" />
+            <h1 className='text-uppercase fw-bold text-light'>Start Bootstrap</h1>
+            <p className='text-light'>Graphic Artist - Web Designer - Illustrator</p>
+        </div>
+    </div>
+    </>
+}
+
+export default Hero
